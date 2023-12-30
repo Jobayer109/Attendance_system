4be830fa3800952bfa5e2667c30ce1c9e9dc928c
user.email <jobayer.ahmed109@gmail.com>
2023-12-30 17:32:05 +0600

    Database connection and user data storing to database has done

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+const connectDB = require("./db");
+const User = require("./models/User");
 
+// Middlewares
+app.use(express.json());
+
+// Home page
 app.get("/", (_req, res) => {
   res.send("Server is running");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// Register
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+  user = new User({ name, email, password });
+  await user.save();
+  res.status(201).send({ message: "User created successfully", user });
+});
+
+// Database connection
+connectDB("mongodb://127.0.0.1:27017/Attendance-DB").then(() => {
+  console.log("Database connected");
+  app.listen(port, () => {
+    console.log(`Server is running on port: https://localhost:${port}`);
+  });
 });
