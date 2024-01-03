6059670ae661a5f19384f49ebb073282e9a1ffad
user.email <jobayer.ahmed109@gmail.com>
2024-01-03 23:52:43 +0600

    Initial routes created for CRUD operation

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
+const userRoutes = require("../routes/user");
 
+router.use("/api/v1/users", userRoutes);
 router.use("/api/v1", authRoutes);
 router.use("/api/v1/auth", authRoutes);
 
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+// Get user by ID
+router.get("/:userId", () => {});
+
+// Get all users
+router.get("/", () => {});
+
+// Create new user
+router.post("/", () => {});
+
+// Update all info of an user
+router.put("/:userId", () => {});
+
+// Update single portion of info
+router.patch("/:userId", () => {});
+
+// Delete user by  ID
+router.delete("/:userId", () => {});
+
+module.exports = router;
