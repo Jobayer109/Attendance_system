36debf9896c06d988047e522c37a841a93f28e58
user.email <jobayer.ahmed109@gmail.com>
2024-01-06 20:16:35 +0600

    Admin attendance routes created

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const authRoutes = require("./auth");
 const userRoutes = require("../routes/user");
 const authenticate = require("../middlewares/authenticate");
+const adminAttendanceRoutes = require("../routes/admin-attendance");
 
-router.use("/api/v1/users", authenticate, userRoutes);
 router.use("/api/v1", authRoutes);
 router.use("/api/v1/auth", authRoutes);
+router.use("/api/v1/users", authenticate, userRoutes);
+router.use("/api/v1/admin", authenticate, adminAttendanceRoutes);
 
 module.exports = router;
